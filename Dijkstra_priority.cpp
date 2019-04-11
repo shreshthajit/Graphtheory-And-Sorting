79c0b15666a2f6db7cad450081ba267df2f95200
Shreshthajit Das <43321488+shreshthajit@users.noreply.github.com>
2019-04-11 15:00:11 +0600

    Update Dijkstra_priority.cpp

--- Dijkstra_priority.cpp
+++ Dijkstra_priority.cpp
@@ -85,3 +85,26 @@ is equivalent to
 
 if ( x != 0 )
 */
+
+
+/*continue statement
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 10; ++i)
+    {
+        if ( i == 6 || i == 9)
+        {
+            continue;
+        }
+        cout << i << "\t";
+    }
+    return 0;
+}
+Output
+
+1	2	3	4	5      7	8	10
+*/
